f745c4c27d73104c57514625bf6570b570edb800
Aiden <marcin.madry@interia.com>
2022-12-27 18:04:47 +0100

    Static build of SDL2

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ REM Change this to your visual studio's 'vcvars64.bat' script path
 set MSVC_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build"
 set CXXFLAGS=/std:c++17 /EHsc /W4 /WX /FC /wd4996 /nologo %*
 set INCLUDES=/I"deps\include"
-set LIBS="deps\lib\SDL2.lib" "deps\lib\SDL2main.lib" shell32.lib
+set LIBS="deps\lib\SDL2.lib" "deps\lib\SDL2main.lib" shell32.lib gdi32.lib Advapi32.lib	Ole32.lib	OleAut32.lib
 
 call %MSVC_PATH%\vcvars64.bat
 
